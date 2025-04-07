8e645d5084504183d4800cbc2434c98aff2a0cdd
Joanna Keza <joanna.keza@stu.ocsb.ca>
2025-04-07 18:13:06 +0000

    fare.py

--- fare.py
+++ fare.py
@@ -10,22 +10,29 @@ import constants_fare
 
 
 def main():
-    age_integer = input("Enter your age: ")
+    age_string = input("Enter your age: ")
     day_integer = input("What day is today (eg; Monday, Thursday): ")
-    bus_system = input("Which transit system are you using today (OC Transpo/TTC Toronto): ")
+    bus_system_integer = input("Which transit system are you using today (OC Transpo/TTC Toronto): ")
 
+    try:
+        age_integer = int(age_string)
+        day_string = str(day_integer)
+        bus_system_string = str(bus_system_integer)
+        if bus_system_string == "OC Transpo":
+            if (age_integer > 10) and (age_integer < 65):
+                print("Your fare is ${}".format(constants_fare.OC_ADULT))
+            elif age_integer > 65:
+                if day_string == "Wednesday" or day_string == "Sunday":
+                    print("There is no fare! It is free, have a great day!")
+                else:
+                    print("Your fare is ${}".format(constants_fare.OC_SENIOR))
+            if age_integer < 10:
+                print("There is no fare! It is free, have a great day!")
 
-    if bus_system == "OC Transpo":
-        if age_integer > 19 and age_integer < 65:
-            print("Your fare is ${}".format(constants_fare.OC_ADULT))
-        else:
-            print("Your fare is ${}".format(constants_fare.OC_STUDENT))
-        print("")
-    elif age_integer > 65:
-        if day_integer == "Wednesday" or day_integer == "Sunday":
-            print("There is no fare! It is free, have a great day!")
-        else:
-            print("Your fare is ${}".format(constants_fare.OC_SENIOR))
+    except ValueError:
+        print("{} is not a valid input".format(age_string))
+        print("{} is not a valid input".format(day_integer))
+        print("{} is not a valid input".format(bus_system_integer))
 
 if __name__ == "__main__":
     main()
