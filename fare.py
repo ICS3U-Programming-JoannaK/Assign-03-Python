e1c5f2f23146e98db5e28380b584bd121f6de8af
Joanna Keza <joanna.keza@stu.ocsb.ca>
2025-04-07 21:49:45 +0000

    fare.py

--- fare.py
+++ fare.py
@@ -11,28 +11,31 @@ import constants_fare
 
 def main():
     age_string = input("Enter your age: ")
-    day_integer = input("What day is today (eg; Monday, Thursday): ")
-    bus_system_integer = input("Which transit system are you using today (OC Transpo/TTC Toronto): ")
 
     try:
         age_integer = int(age_string)
-        day_string = str(day_integer)
-        bus_system_string = str(bus_system_integer)
-        if bus_system_string == "OC Transpo":
-            if (age_integer > 10) and (age_integer < 65):
-                print("Your fare is ${}".format(constants_fare.OC_ADULT))
-            elif age_integer > 65:
-                if day_string == "Wednesday" or day_string == "Sunday":
-                    print("There is no fare! It is free, have a great day!")
+        day_string = input("What day is today (eg; Monday, Thursday): ")
+        bus_system_integer = input(
+            "Which transit system are you using today (OC Transpo/TTC Toronto): "
+        )
+        try:
+            bus_system_string = str(bus_system_integer)
+            if bus_system_string == "OC Transpo":
+                if (age_integer > 10) and (age_integer < 65):
+                    print("Your fare is ${}".format(constants_fare.OC_ADULT))
+                elif age_integer > 65:
+                    if (day_string == "Wednesday") or (day_string == "Sunday"):
+                        print("There is no fare! It is free, have a great day!")
                 else:
                     print("Your fare is ${}".format(constants_fare.OC_SENIOR))
             if age_integer < 10:
                 print("There is no fare! It is free, have a great day!")
+        except Exception:
+            print("{} is not a valid input".format(bus_system_integer))
 
-    except ValueError:
+    except Exception:
         print("{} is not a valid input".format(age_string))
-        print("{} is not a valid input".format(day_integer))
-        print("{} is not a valid input".format(bus_system_integer))
+
 
 if __name__ == "__main__":
     main()
