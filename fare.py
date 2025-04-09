d96552b021c1fe074bd9dccf7229f2aa8a509677
Joanna Keza <joanna.keza@stu.ocsb.ca>
2025-04-09 23:32:52 +0000

    fare.py

--- fare.py
+++ fare.py
@@ -14,6 +14,8 @@ def main():
 
     try:
         age_integer = int(age_string)
+        if age_integer < 1:
+            print("Please enter a valid age")
         day_string = input("What day is today (eg; Monday, Thursday): ")
         bus_system_integer = input(
             "Which transit system are you using today (OC Transpo/TTC Toronto): "
@@ -30,21 +32,22 @@ def main():
                     print("Your fare is ${}".format(constants_fare.OC_SENIOR))
             if age_integer < 10:
                 print("There is no fare! It is free, have a great day!")
+            if bus_system_string == "TTC Toronto":
+                if (age_integer >= 19) and (age_integer < 65):
+                    print("Your fare is ${}".format(constants_fare.TTC_ADULT))
+                elif age_integer >= 65:
+                    print("Your fare is ${}".format(constants_fare.TTC_SENIOR))
+                elif (age_integer > 13) and (age_integer < 18):
+                    print("Your fare is ${}".format(constants_fare.TTC_STUDENT))
+                elif age_integer <= 12:
+                    print("There is no fare! It is free, have a great day!")
         except Exception:
             print("{} is not a valid input".format(bus_system_integer))
+        finally:
+            print("Thank you for using my program, I hope you have a wonderful day !")
 
     except Exception:
         print("{} is not a valid input".format(age_string))
-
-    if bus_system_string == "TTC Toronto":
-        if (age_integer > 12) and (age_integer < 65):
-            print("Your fare is $()".format(constants_fare.TTC_ADULT))
-        elif age_integer > 65:
-            print("Your fare is ${}".format(constants_fare.TTC_SENIOR))
-        elif (age_integer > 13) and (age_integer < 19):
-            print("Your fare is ${}".format(constants_fare.TTC_STUDENT))
-        elif age_integer < 12:
-            print("There is no fare! It is free, have a great day!")
 
 
 if __name__ == "__main__":
